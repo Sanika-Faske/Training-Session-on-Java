ee86c3d5c7abcb3c2732f11c683ad2a0b6f2733d
Sanika <143709223+Sanika-Faske@users.noreply.github.com>
2024-06-11 15:19:14 +0530

    Add files via upload

---
+++ Arithmetic_opeartor.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Arithmetic_opeartor {
+	public static void main(String args[]){
+		int a = 10;
+		int b = 3;
+		System.out.println("a + b = "+(a+b));
+		System.out.println("a - b = "+(a-b));
+		System.out.println("a * b = "+(a*b));
+		System.out.println("a / b = "+(a/b));
+		System.out.println("a % b = "+(a%b));
+	}
+
+}
