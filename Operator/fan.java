9cb4ffb6d5024fffa117a64dda5764646e8df3ad
Sanika <143709223+Sanika-Faske@users.noreply.github.com>
2024-06-18 10:37:26 +0530

    Add files via upload

---
+++ Operator/fan.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class fan {
+
+	public static void main(String[] args) {
+		
+		int i_val=9;
+		if(i_val%2==0) {
+			System.out.println("Even No");	
+		}else {
+			System.out.println("Odd No");
+		}
+		
+
+	}
+
+}
